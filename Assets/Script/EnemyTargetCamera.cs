dfe233042cecf980fc05aa741f1c4886fb6f8ea5
Mete <109483424+meteulken@users.noreply.github.com>
2023-04-21 01:51:34 +0300

    changes.

---
+++ Assets/Script/EnemyTargetCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+namespace MimicSpace
+{
+    public class EnemyTargetCamera : MonoBehaviour
+    {
+        
+
+        private void Start()
+        {
+           
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
