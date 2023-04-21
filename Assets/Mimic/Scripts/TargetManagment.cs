8a33a1c9b649519015868a60443f34d07e757e3c
Mete <109483424+meteulken@users.noreply.github.com>
2023-04-21 03:36:38 +0300

    Camera Target add.

---
+++ Assets/Mimic/Scripts/TargetManagment.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MimicSpace;
+
+public class TargetManagment : MonoBehaviour
+{
+    public GameObject target;
+    public Camera cam;
+
+    private bool IsVisible(Camera c, GameObject Target)
+    {
+        Vector3 screenPoint = c.WorldToViewportPoint(Target.transform.position);
+        return screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+    }
+
+    // Movement script'inden erişeceğimiz nesne
+    public Movement mimicMovement;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (IsVisible(cam, target))
+        {
+            mimicMovement.speed = 10f; // Hızlandırma
+        }
+        else
+        {
+            mimicMovement.speed = 4f; // Yavaşlatma
+        }
+    }
+}
